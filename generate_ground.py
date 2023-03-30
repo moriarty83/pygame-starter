7a0a9cdca062838e9a7760b89c63985fa086ca62
moriarty83 <moriarty83@gmail.com>
2023-03-30 14:51:28 -0400

    render and rotate ship

---
+++ generate_ground.py
@@ -0,0 +1,122 @@
+import pygame
+import os
+import random
+
+pygame.init()
+
+ground = pygame.display.set_mode((1280, 720))
+
+
+ground_tiles_image = pygame.image.load(os.path.join(
+    'assets/spritesheets', 'groundtiles_grayscale.png')).convert_alpha()
+
+tiles = {
+    "flat": [32, 0],
+    "up_steep": [224, 0],
+    "down_steep": [192, 0],
+    "up_gentle_start": [192, 64],
+    "down_gentle_start": [128, 64],
+    "up_gentle_continue": [224, 64],
+    "down_gentle_continue": [160, 64],
+    "inside": [128, 0]
+}
+
+up_flat_tiles = {
+    "flat": [32, 0],
+    "up_steep": [224, 0],
+    "up_gentle_start": [192, 64],
+}
+
+down_flat_tiles = {
+    "flat": [32, 0],
+    "down_steep": [192, 0],
+    "down_gentle_start": [128, 64],
+}
+
+
+def get_ground_tile(sheet, type):
+
+    # Types:
+    width = 32
+    height = 32
+
+    image = pygame.Surface((width, height))
+    image = image.convert_alpha()
+    image.set_colorkey("black")
+    # blit argument 1: image, 2: reder coords, 3: coords from spriteshhet
+    image.blit(sheet, (0, 0), (tiles[type][0], tiles[type]
+               [1], tiles[type][0]+32, tiles[type][1]+32))
+    return image
+
+
+def build_ground_col(rows):
+
+    global makingRow
+    makingRow = True
+
+    toppings = dict(list(tiles.items())[:5])
+    height = 8
+    topper = random.choice(list(toppings.keys()))
+    firstRow = False if len(rows) > 0 else True
+    if firstRow == False:
+        height = rows[-1]["height"]
+        if height > 14:
+            topper = random.choice(list(down_flat_tiles.keys()))
+        if height < 4:
+            topper = random.choice(list(up_flat_tiles.keys()))
+
+        proceed = True if random.randint(
+            0, 10) > 3 and height < 15 and height > 3 else False
+        gentleSlope = True if firstRow == False and rows[-1]["topper"] == "up_gentle_start" or firstRow == False and rows[-1]["topper"] == "down_gentle_start" else False
+        if rows[-1]["topper"] == "up_gentle_start":
+            topper = "up_gentle_continue"
+        elif rows[-1]["topper"] == "down_gentle_start":
+            topper = "down_gentle_continue"
+
+        elif proceed == True or gentleSlope == True:
+
+            if rows[-1]["topper"] == "up_steep":
+                topper = "up_steep"
+                height += 1
+            elif rows[-1]["topper"] == "down_steep":
+                topper = "down_steep"
+                height -= 1
+
+            elif rows[-1]["topper"] == "up_gentle_continue":
+                height += 1
+                topper = "up_gentle_start"
+            elif rows[-1]["topper"] == "down_gentle_continue":
+                height -= 1
+                topper = "down_gentle_start"
+            else:
+                topper = rows[-1]["topper"]
+            if height < 3:
+                topper = "flat"
+                height = 3
+            if height > 15:
+                topper = "flat"
+                height = 15
+
+        elif proceed == False and firstRow == False:
+            if rows[-1]["topper"] == "flat":
+                if topper == "up_steep" or topper == "up_gentle_start":
+                    height += 1
+            elif rows[-1]["topper"] == "up_steep" or rows[-1]["topper"] == "up_gentle_continue":
+                if topper == "up_steep" or topper == "up_gentle_start":
+                    height += 1
+            elif rows[-1]["topper"] == "down_steep" or rows[-1]["topper"] == "down_gentle_continue":
+                if topper != "up_steep" and topper != "up_gentle_start":
+
+                    height -= 1
+
+    surface = pygame.Surface((32, height*32))
+    for i in range(height, -1, -1):
+        if i != 0:
+            type = "inside"
+        else:
+            type = topper
+        tile = get_ground_tile(ground_tiles_image, type)
+        surface.blit(tile, (0, i*32))
+    col = {"surface": surface, "height": height, "topper": topper}
+
+    return col
--- quickstart.py
+++ quickstart.py
@@ -1,32 +1,49 @@
 import pygame
 from pygame.locals import *
+
+import moderngl
+import numpy as np
+
 import os
 import sys
 import math
-import random
-import time
+from generate_ground import *
+
 
 pygame.init()
 
-makingRow = False
 
 screen = pygame.display.set_mode((1280, 720))
 ground = pygame.display.set_mode((1280, 720))
 pygame.display.set_caption("Lander")
+
+scroll_x_speed = 2.4
+scroll_y_speed = 2.4
+
+scroll_height = -1000
+player_altitude = 1000
+player_rotation_current = 0
+player_rotation_new = 0
+
 
 # BACKGROUND
 background_01 = pygame.image.load(os.path.join(
     'assets/backgrounds', 'spr_stars01.png')).convert()
 background_01X = 0
 background_01X2 = background_01.get_width()
+background_01Y = 0
+background_01Y2 = background_01.get_height()
 
 # BACKGROUND 2
-xspeed = 2.4
+
 background_02 = pygame.image.load(os.path.join(
     'assets/backgrounds', 'spr_stars01.png')).convert_alpha()
 background_02 = pygame.transform.scale_by(background_02, 2)
 background_02X = 0
 background_02X2 = background_02.get_width()
+background_02Y = 0
+background_02Y2 = background_02.get_height()
+
 
 # GROUND
 rows = []
@@ -34,28 +51,7 @@ ground_tiles_image = pygame.image.load(os.path.join(
     'assets/spritesheets', 'groundtiles_grayscale.png')).convert_alpha()
 groundX = 0
 groundX_02 = screen.get_width()
-tiles = {
-    "flat": [32, 0],
-    "up_steep": [224, 0],
-    "down_steep": [192, 0],
-    "up_gentle_start": [192, 64],
-    "down_gentle_start": [128, 64],
-    "up_gentle_continue": [224, 64],
-    "down_gentle_continue": [160, 64],
-    "inside": [128, 0]
-}
 
-up_flat_tiles = {
-    "flat": [32, 0],
-    "up_steep": [224, 0],
-    "up_gentle_start": [192, 64],
-}
-
-down_flat_tiles = {
-    "flat": [32, 0],
-    "down_steep": [192, 0],
-    "down_gentle_start": [128, 64],
-}
 
 clock = pygame.time.Clock()
 running = True
@@ -63,43 +59,93 @@ dt = 0
 
 first_load = True
 
-lines = []
-
 # CREATE PLAYER
 player_pos = pygame.Vector2(screen.get_width() / 2, screen.get_height() / 2)
+player_sprites = [pygame.image.load(os.path.join(
+    'assets/spritesheets', 'lander_03_00.png')).convert_alpha(), pygame.image.load(os.path.join(
+        'assets/spritesheets', 'lander_03_15.png')).convert_alpha(), pygame.image.load(os.path.join(
+            'assets/spritesheets', 'lander_03_30.png')).convert_alpha(), pygame.image.load(os.path.join(
+                'assets/spritesheets', 'lander_03_45.png')).convert_alpha(), pygame.image.load(os.path.join(
+                    'assets/spritesheets', 'lander_03_60.png')).convert_alpha(), pygame.image.load(os.path.join(
+                        'assets/spritesheets', 'lander_03_75.png')).convert_alpha(), pygame.image.load(os.path.join(
+                            'assets/spritesheets', 'lander_03_90.png')).convert_alpha(), pygame.image.load(os.path.join(
+                                'assets/spritesheets', 'lander_03_105.png')).convert_alpha(), pygame.image.load(os.path.join(
+                                    'assets/spritesheets', 'lander_03_120.png')).convert_alpha(), pygame.image.load(os.path.join(
+                                        'assets/spritesheets', 'lander_03_135.png')).convert_alpha(), pygame.image.load(os.path.join(
+                                            'assets/spritesheets', 'lander_03_150.png')).convert_alpha(), pygame.image.load(os.path.join(
+                                                'assets/spritesheets', 'lander_03_165.png')).convert_alpha(), pygame.image.load(os.path.join(
+                                                    'assets/spritesheets', 'lander_03_180.png')).convert_alpha(), pygame.image.load(os.path.join(
+                                                        'assets/spritesheets', 'lander_03_195.png')).convert_alpha(), pygame.image.load(os.path.join(
+                                                            'assets/spritesheets', 'lander_03_210.png')).convert_alpha(), pygame.image.load(os.path.join(
+                                                                'assets/spritesheets', 'lander_03_225.png')).convert_alpha(), pygame.image.load(os.path.join(
+                                                                    'assets/spritesheets', 'lander_03_240.png')).convert_alpha(), pygame.image.load(os.path.join(
+                                                                        'assets/spritesheets', 'lander_03_255.png')).convert_alpha(), pygame.image.load(os.path.join(
+                                                                            'assets/spritesheets', 'lander_03_270.png')).convert_alpha(), pygame.image.load(os.path.join(
+                                                                                'assets/spritesheets', 'lander_03_285.png')).convert_alpha(), pygame.image.load(os.path.join(
+                                                                                    'assets/spritesheets', 'lander_03_300.png')).convert_alpha(), pygame.image.load(os.path.join(
+                                                                                        'assets/spritesheets', 'lander_03_315.png')).convert_alpha(), pygame.image.load(os.path.join(
+                                                                                            'assets/spritesheets', 'lander_03_330.png')).convert_alpha(), pygame.image.load(os.path.join(
+                                                                                                'assets/spritesheets', 'lander_03_345.png')).convert_alpha()]
 
-# SET START GROUND COORDINATE
-ground_start = pygame.Vector2(0, screen.get_height()*3 / 4)
+for sprite in player_sprites:
+    sprite = pygame.transform.scale_by(sprite, 2)
+    sprite.set_colorkey("black")
 
 
 def redrawWindow():
+    global player_sprites
+    global player_rotation_current
+    global player_rotation_new
     # draws our first background_01 image
-    screen.blit(background_01, (background_01X, 0))
-    # draws the seconf background_01 image
-    screen.blit(background_01, (background_01X2, 0))
-    screen.blit(background_02, (background_02X, 0))
+    screen.blit(background_01, (background_01X, background_01Y))
+    screen.blit(background_01, (background_01X2, background_01Y))
+
+    screen.blit(background_01, (background_01X, background_01Y2))
+    screen.blit(background_01, (background_01X2, background_01Y2))
+
+    # screen.blit(background_02, (background_02X, background_02Y))
     # # draws the seconf background_02 image
-    screen.blit(background_02, (background_02X2, 0))
+    # screen.blit(background_02, (background_02X2, background_02Y2))
     for i in range(len(rows)):
         screen.blit(
-            rows[i]['surface'], (i*32+groundX, screen.get_height()-rows[i]['surface'].get_height()))
-    # pygame.display.update()  # updates the screen
+            rows[i]['surface'], (i*32+groundX, screen.get_height()-rows[i]['surface'].get_height()+(scroll_height*-1)))
+
+    active_player_sprite = math.floor(player_rotation_current % 360 / 15)
+
+    screen.blit(player_sprites[active_player_sprite], (player_pos))
+    pygame.display.update()  # updates the screen
+
+# screen.get_height()-rows[i]['surface'].get_height()
 
 
 def updateBackground():
+    global player_rotation
+
     global background_01X
     global background_01X2
     global background_02X
     global background_02X2
+
+    global background_01Y
+    global background_01Y2
+    global background_02Y
+    global background_02Y2
+
     global groundX
 
-    background_01X -= xspeed
-    background_01X2 -= xspeed
-    background_02X -= xspeed * 2
-    background_02X2 -= xspeed * 2
-    groundX -= xspeed * 2.5
+    background_01X -= scroll_x_speed
+    background_01X2 -= scroll_x_speed
+    background_02X -= scroll_x_speed * 2
+    background_02X2 -= scroll_x_speed * 2
 
-    if xspeed > 0:
+    background_01Y -= scroll_y_speed
+    background_01Y2 -= scroll_y_speed
+    background_02Y -= scroll_y_speed * 2
+    background_02Y2 -= scroll_y_speed * 2
+
+    groundX -= scroll_x_speed * 2.5
+
+    if scroll_x_speed > 0:
         if background_01X < background_01.get_width() * -1:
             background_01X = background_01.get_width()
         if background_01X2 < background_01.get_width() * -1:
@@ -118,123 +164,24 @@ def updateBackground():
         if background_02X2 > background_02.get_width() * 1:
             background_02X2 = -background_02.get_width()
 
-
-def get_ground_tile(sheet, type):
-
-    # Types:
-    width = 32
-    height = 32
-
-    image = pygame.Surface((width, height))
-    image = image.convert_alpha()
-    image.set_colorkey("black")
-    # blit argument 1: image, 2: reder coords, 3: coords from spriteshhet
-    image.blit(sheet, (0, 0), (tiles[type][0], tiles[type]
-               [1], tiles[type][0]+32, tiles[type][1]+32))
-    return image
-
-
-def generate_new_col(forward=True):
-    previousCol = rows[-1]
-
-
-def build_ground_col():
-
-    global makingRow
-    makingRow = True
-
-    toppings = dict(list(tiles.items())[:5])
-    height = 8
-    topper = random.choice(list(toppings.keys()))
-    firstRow = False if len(rows) > 0 else True
-    if firstRow == False:
-        height = rows[-1]["height"]
-        if height > 14:
-            print("hit height max")
-            topper = random.choice(list(down_flat_tiles.keys()))
-        if height < 4:
-            print("hit height min")
-            topper = random.choice(list(up_flat_tiles.keys()))
-
-        proceed = True if random.randint(
-            0, 10) > 3 and height < 15 and height > 3 else False
-        gentleSlope = True if firstRow == False and rows[-1]["topper"] == "up_gentle_start" or firstRow == False and rows[-1]["topper"] == "down_gentle_start" else False
-        if rows[-1]["topper"] == "up_gentle_start":
-            print("hit 1")
-            topper = "up_gentle_continue"
-        elif rows[-1]["topper"] == "down_gentle_start":
-            print("hit 2")
-            topper = "down_gentle_continue"
-
-        elif proceed == True or gentleSlope == True:
-            print("hit 3")
-
-            if rows[-1]["topper"] == "up_steep":
-                print("hit 4")
-                topper = "up_steep"
-                height += 1
-            elif rows[-1]["topper"] == "down_steep":
-                print("hit 5")
-                topper = "down_steep"
-                height -= 1
-
-            elif rows[-1]["topper"] == "up_gentle_continue":
-                print("hit 6")
-                height += 1
-                topper = "up_gentle_start"
-            elif rows[-1]["topper"] == "down_gentle_continue":
-                print("hit 7")
-                height -= 1
-                topper = "down_gentle_start"
-            else:
-                print("hit 8")
-                topper = rows[-1]["topper"]
-            if height < 3:
-                print("hit 9")
-                topper = "flat"
-                height = 3
-            if height > 15:
-                print("hit 10")
-                topper = "flat"
-                height = 15
-
-        elif proceed == False and firstRow == False:
-            print("proceed false, topper is: ", topper)
-            if rows[-1]["topper"] == "flat":
-                print("hit 11")
-                if topper == "up_steep" or topper == "up_gentle_start":
-                    print("hit 12")
-                    height += 1
-            elif rows[-1]["topper"] == "up_steep" or rows[-1]["topper"] == "up_gentle_continue":
-                print("hit 13")
-                if topper == "up_steep" or topper == "up_gentle_start":
-                    print("hit 14")
-                    height += 1
-            elif rows[-1]["topper"] == "down_steep" or rows[-1]["topper"] == "down_gentle_continue":
-                print("hit 15")
-                if topper != "up_steep" and topper != "up_gentle_start":
-                    print("hit 16")
-                    print("topper is NOT upsteep, up gentle, topper is: {}",
-                          topper)
-                    height -= 1
-                else:
-                    print("hit 17")
-                    print(
-                        "topper is upsteep, up gentle, topper is: {}", topper)
-        print("previous topper is " + rows[-1]["topper"] + " previous height is " +
-              str(rows[-1]["height"]) + " current topper is " + topper)
-
-    surface = pygame.Surface((32, height*32))
-    for i in range(height):
-        if i != 0:
-            type = "inside"
-        else:
-            type = topper
-        tile = get_ground_tile(ground_tiles_image, type)
-        surface.blit(tile, (0, i*32))
-    col = {"surface": surface, "height": height, "topper": topper}
-
-    return col
+    if scroll_y_speed > 0:
+        if background_01Y < background_01.get_height() * -1:
+            background_01Y = background_01.get_height()
+        if background_01Y2 < background_01.get_height() * -1:
+            background_01Y2 = background_01.get_height()
+        if background_02Y < background_02.get_height() * -1:
+            background_02Y = background_02.get_height()
+        if background_02Y2 < background_02.get_height() * -1:
+            background_02Y2 = background_02.get_height()
+    else:
+        if background_01Y > background_01.get_height() * 1:
+            background_01Y = -background_01.get_height()
+        if background_01Y2 > background_01.get_height() * 1:
+            background_01Y2 = -background_01.get_height()
+        if background_02Y > background_02.get_height() * 1:
+            background_02Y = -background_02.get_height()
+        if background_02Y2 > background_02.get_height() * 1:
+            background_02Y2 = -background_02.get_height()
 
 
 while running:
@@ -243,32 +190,36 @@ while running:
 
     if first_load:
         for i in range(math.ceil(screen.get_width()/32)):
-            rows.append(build_ground_col())
+            rows.append(build_ground_col(rows))
         first_load = False
-    if len(rows) * 32 < screen.get_width()/2 + groundX*-1:
-        rows.append(build_ground_col())
+    if len(rows) * 32 < screen.get_width()-32 + groundX*-1:
+        rows.append(build_ground_col(rows))
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
     keys = pygame.key.get_pressed()
-    if keys[pygame.K_j]:
-        if makingRow != True:
-            rows.append(build_ground_col())
 
-    makingRow = False
     if keys[pygame.K_w]:
-        player_pos.y -= 300 * dt
+        scroll_y_speed -= 1 * dt
 
     if keys[pygame.K_s]:
-        player_pos.y += 300 * dt
+        scroll_y_speed += 1 * dt
     if keys[pygame.K_a]:
-        xspeed -= 1 * dt
+        player_rotation_current -= 15
 
     if keys[pygame.K_d]:
-        xspeed += 1 * dt
+        player_rotation_current += 15
 
-    pygame.display.flip()
+    if (scroll_height >= 0):
+        scroll_height = 0
+        scroll_y_speed = 0 if scroll_y_speed >= 0 else scroll_y_speed
+    scroll_height += scroll_y_speed
+    player_altitude -= scroll_y_speed
+
+    pygame.display.update()
+    print("pr_current:", player_rotation_current)
+    print("pr_new:", player_rotation_new)
 
     dt = clock.tick(60) / 1000
 
